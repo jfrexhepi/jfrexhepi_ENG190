39b7309ccd48e8e306e791f3b5297cb8147c125b
jfrexhepi <frexhepi@student.ccc.edu>
2026-02-11 21:19:29 -0600

    Refactor diagonalLine.py for clarity and efficiency

--- diagonalLine.py.py
+++ diagonalLine.py.py
@@ -20,14 +20,6 @@ for j in range(100) :
            # draw white
            draw.point((i,j), fill='blue')
            newLine[i] = 0
-        # Here we rendering the currentLine
-        
-       # if aboveLine[i] % 2 == 0 :
-       #     draw.point((i,j), fill='black')
-       #     newLine[i] = 1
-       # else :
-       #     draw.point((i,j), fill='white')
-       #     newLine[i] = 0
     for i in range(50,99) :
         if (aboveLine[i-1] == 1 ) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 0) or (aboveLine[i-1] == 0 ) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 1) :
             # // draw black
@@ -37,23 +29,33 @@ for j in range(100) :
            # draw white
            draw.point((i,j), fill='blue')
            newLine[i] = 0
-        # Here we rendering the currentLine
-        
-       # if aboveLine[i] % 2 == 0 :
-       #     draw.point((i,j), fill='black')
-       #     newLine[i] = 1
-       # else :
-       #     draw.point((i,j), fill='white')
-       #     newLine[i] = 0
     aboveLine = newLine
    
 aboveLine = [0] * 100
-aboveLine[50] = 1
+aboveLine[0] = 1
 
 print(aboveLine)
  #btm right
  
-for j in range(1,50) :
+for j in range(50,100) :
+    newLine = [0] * 100
+    for i in range(0,99) :
+        if (aboveLine[i-1] == 1 ) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 0) or (aboveLine[i-1] == 0) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 1) :
+            # // draw black
+            draw.point((i,j), fill='red')
+            newLine[i] = 1
+        else :
+           # draw white
+           draw.point((i,j), fill='yellow')
+           newLine[i] = 0
+    aboveLine = newLine   
+    
+aboveLine = [0] * 100
+aboveLine[99] = 1
+
+print(aboveLine)
+#btm right
+for j in range(50,100) :
     newLine = [0] * 100
     for i in range(50,99) :
         if (aboveLine[i-1] == 0 ) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 1) or (aboveLine[i-1] == 1 ) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 0) :
@@ -64,39 +66,7 @@ for j in range(1,50) :
            # draw white
            draw.point((i,j), fill='yellow')
            newLine[i] = 0
-        # Here we rendering the currentLine
-        
-       # if aboveLine[i] % 2 == 0 :
-       #     draw.point((i,j), fill='black')
-       #     newLine[i] = 1
-       # else :
-       #     draw.point((i,j), fill='white')
-       #     newLine[i] = 0
-    aboveLine = newLine   
-aboveLine = [0] * 100
-aboveLine[99] = 1
-
-print(aboveLine)
- #btm left
-for j in range(50,99) :
-    newLine = [0] * 100
-    for i in range(50,99) :
-        if (aboveLine[i-1] == 1 ) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 0) or (aboveLine[i-1] == 0 ) and  (aboveLine[i] == 0) and (aboveLine[i+1] == 1) :
-            # // draw black
-            draw.point((i,j), fill='red')
-            newLine[i] = 1
-        else :
-           # draw white
-           draw.point((i,j), fill='yellow')
-           newLine[i] = 0
-        # Here we rendering the currentLine
-        
-       # if aboveLine[i] % 2 == 0 :
-       #     draw.point((i,j), fill='black')
-       #     newLine[i] = 1
-       # else :
-       #     draw.point((i,j), fill='white')
-       #     newLine[i] = 0
     aboveLine = newLine
     
+
 img.show()
